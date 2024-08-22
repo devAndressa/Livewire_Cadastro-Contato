d4dc7097a01f3cb00d301017d1948af83008e781
sophialohanny@gmail.com <sophialohanny@gmail.com>
2024-08-22 11:20:13 -0300

    aprendendo um pouco mais de livewire

---
+++ resources/views/livewire/contact-create.blade.php
@@ -0,0 +1,55 @@
+<div>
+    <h2>Criar Contato</h2>
+
+<div>
+    <form wire:submit.prevent="create" method="post">
+        <div>
+            @if (session()->has('message'))
+                <div class="alert">
+                    {{ session('message') }}
+                </div>
+            @endif
+        </div>
+
+        <div>
+            <label>Nome</label>
+
+            <input type="text" wire:model="name">
+
+            <div class="error-message">
+                @error('name')
+                    {{$message}}
+                @enderror
+            </div>
+        </div>
+
+        <div>
+            <label>Email</label>
+
+            <input type="email" wire:model="email">
+
+            <div class="error-message">
+                @error('email')
+                    {{$message}}
+                @enderror
+            </div>
+        </div>
+
+        <div>
+            <label>Telefone</label>
+
+            <input type="text" wire:model="phone">
+
+            <div class="error-message">
+                @error('phone')
+                    {{$message}}
+                @enderror
+            </div>
+        </div>
+
+        <button type="submit">
+            Adicionar Contato
+        </button>
+    </form>
+</div>
+</div>
